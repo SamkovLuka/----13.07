64d71dd911b608d8dae588e64f3a1b1d70ba4879
SamkovLuka <staddycase@gmail.com>
2024-07-08 22:58:48 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,110 @@
+import math
+from colorama import init, Fore, Back
+
+
+#init colorama
+init()
+
+#1
+
+#зроблене
+# def printFormtext():
+#     print(f"""{Fore.BLACK}Don't compare yourself with anyone in this world…
+# {Fore.LIGHTMAGENTA_EX}if {Fore.WHITE}you {Fore.MAGENTA} do so{Fore.WHITE}, you are insulting yourself.
+# Bill Gates""")
+# printFormtext()
+
+
+#2
+
+#зроблене
+# def evenNum(a,b):
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+#     for i in range(a,b):
+#         if i%2==0:
+#             print(i)
+#         else:
+#             continue
+
+# evenNum(1,10)
+
+
+
+#3
+
+#не зроблене
+
+
+
+# def printSquare(length, sym, zm):
+#    length = int(input("Enter length of square: "))
+#    sym = input("Enter symbol: ")
+#    zm = input("Enter logical variable: ")
+#    if zm == "True":
+#         sid = sym * length
+#         print(f"\n {sid*length}")
+#    if zm == "True":
+#      sid = sym * length
+#      print(sid*length)
+
+   
+
+# print(printSquare(8, "g", True))
+
+#4
+
+#зроблене
+# def printMin(fi,se,th,fo,ff):
+#     fi = int(input("Enter first number: "))
+#     se = int(input("Enter second number: "))
+#     th = int(input("Enter third number: "))
+#     fo = int(input("Enter fourth number: "))
+#     ff = int(input("Enter fifth number: "))
+#     print(min(fi,se,th,fo,ff))
+
+# print(printMin(1,2,3,4,5))
+
+
+
+
+#5
+#зроблене
+
+
+
+# def printDob(num_a,num_b):
+#     for i in range(num_a,num_b):
+#         if num_a>num_b:
+#             range(num_b,num_a)
+#         print(i*(i+1))
+
+# num_a = int(input("Введіть перше число діапазону: "))
+# num_b = int(input("Введіть друге число діапазону: "))
+# printDob(num_a,num_b)
+
+
+#6
+
+#не зроблене
+
+
+# def countInt(n):
+#     n = list()
+#     n = [n]
+#     n.count(1)
+#     a1 = n.count("1")
+#     a2 = n.count("2")
+#     a3 = n.count("3")
+#     a4 = n.count("4")
+#     a5 = n.count("5")
+#     a6 = n.count("6")
+#     a7 = n.count("7")
+#     a8 = n.count("8")
+#     a9 = n.count("9")
+#     a10 = n.count("0")
+#     print(a1+a2+a3+a4+a5+a6+a7+a8+a9+a10)
+
+# n = int(input("Введіть число: "))
+
+# countInt(n)
